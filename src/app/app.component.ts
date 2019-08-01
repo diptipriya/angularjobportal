9780ab31dff1a99773b3c84cb1f791a748f92148
Dipti Priya <diptipriya@Diptis-MacBook-Air.local>
2019-08-01 11:06:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jobportal';
+export class AppComponent implements OnInit {
+  title = 'indeed';
+ jobsdata;
+ mumbai;
+ pune;
+ 
+
+constructor(public http:HttpClient){
+
+}
+
+ngOnInit(){
+
+}
+
+
+
+getjoball()
+{
+
+  this.http.get('http://localhost:8888/jobportal/readdata.php').subscribe(
+    data => {
+      
+      this.jobsdata = data as string;
+      console.log('Data fetched is successful ', data);
+    },
+    error => {
+      console.log('Error', error);
+
+}
+  );
+}
+
+getjob(mumbai)
+{
+  this.http.post('http://localhost:8888/jobportal/filterdata.php', 
+  {"location": mumbai}).subscribe(
+  
+    data => {
+      this.jobsdata = data as string;
+      console.log('Data fetched is successful ', data);
+    },
+    error => {
+      console.log('Error', error);
+
+}
+  );
+}
+
+
+getjobpune(pune)
+{
+  this.http.post('http://localhost:8888/jobportal/filterdata.php', 
+  {"location": pune}).subscribe(
+  
+    data => {
+      this.jobsdata = data as string;
+      console.log('Data fetched is successful ', data);
+    },
+    error => {
+      console.log('Error', error);
+
+}
+  );
+}
+
 }
